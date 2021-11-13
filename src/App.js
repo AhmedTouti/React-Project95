77b5efcd6b19e7971455b7fa31e5f4e3bcab44dd
AhmedTouti <ahmedtouti.r@gmail.com>
2021-11-13 18:45:54 +0100

    twklna 3la lah

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>this is bismillah</h2>
+      <h3>commit test</h3>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,8 @@
+import React, { component } from 'react'
+
+
+const Home = (props) => {
+    return (<><h1>home</h1></>);
+}
+
+export default Home

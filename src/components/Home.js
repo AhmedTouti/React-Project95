326489d3882e4734cda97693702e4da7b3e4a2d3
AhmedTouti <ahmedtouti.r@gmail.com>
2021-11-13 19:28:24 +0100

    start coding

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,9 @@ import React, { component } from 'react'
 
 
 const Home = (props) => {
-    return (<><h1>home</h1></>);
+    return (<><h1>home</h1>
+    <h2>this when i started coding this project</h2>
+    </>);
 }
 
 export default Home

b30b577cc5a6cc4c9d420432590c3f7687dc1934
AhmedTouti <ahmedtouti.r@gmail.com>
2021-11-14 22:47:54 +0100

    adding components

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React, { component } from 'react'
 
 const Home = (props) => {
     return (<><h1>home</h1>
-    <h2>this when i started coding this project</h2>
+        <h2>this when i started coding this project</h2>
     </>);
 }
 
